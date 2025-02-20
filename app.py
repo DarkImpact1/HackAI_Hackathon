c01fe854569379e40b7bc45a08d0bb5cc7215a3f
Suryansh <raisuryansh4@gmail.com>
2025-02-21 04:29:34 +0530

    add fracture detector

--- app.py
+++ app.py
@@ -1,90 +1,118 @@
 import streamlit as st
-from module import predict_image
+from module import predict_pneumonia_image, classify_fracture
 import os
 
 # Set Streamlit page configuration
-st.set_page_config(
-    page_title="Medi Assist - Pneumonia Detection",
-    page_icon="🩺",
-    layout="wide"
-)
+def configure_page():
+    st.set_page_config(
+        page_title="Medi Assist - Pneumonia Detection",
+        page_icon="🩺",
+        layout="wide"
+    )
+
 
 # ---- TOP BAR ----
-st.markdown(
-    """
-    <style>
-        .top-bar {
-            background-color: #0a66c2;
-            color: white;
-            padding: 10px;
-            text-align: center;
-            font-size: 18px;
-            font-weight: bold;
-            border-radius: 5px;
-        }
-        .top-bar a {
-            color: white;
-            text-decoration: none;
-            font-weight: bold;
-        }
-    </style>
-    <div class="top-bar">
-        Developed by <a href="https://www.linkedin.com/in/mohit-dwivedi13" target="_blank">Mohit Dwivedi</a> 🩺
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+def top_bar():
+    st.markdown(
+        """
+        <style>
+            .top-bar {
+                background-color: #0a66c2;
+                color: white;
+                padding: 10px;
+                text-align: center;
+                font-size: 18px;
+                font-weight: bold;
+                border-radius: 5px;
+            }
+            .top-bar a {
+                color: white;
+                text-decoration: none;
+                font-weight: bold;
+            }
+        </style>
+        <div class="top-bar">
+            Developed by <a href="https://www.linkedin.com/in/mohit-dwivedi13" target="_blank">Mohit Dwivedi</a> 🩺
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
-# ---- SIDEBAR ----
-st.sidebar.header("About This App")
-st.sidebar.write("This AI-powered tool detects **Pneumonia** from chest X-ray images using Deep Learning.")
+    # ---- SIDEBAR ----
+    st.sidebar.header("About This App")
+    st.sidebar.write("This AI-powered tool detects **Pneumonia** from chest X-ray images using Deep Learning.")
 
-st.sidebar.header("How to Use")
-st.sidebar.write("""
-1. Upload a **chest X-ray image** 📤  
-2. Wait for the **AI model to analyze** 🧠  
-3. Get a **prediction along with confidence score** 🏥  
-""")
+    st.sidebar.header("How to Use")
+    st.sidebar.write("""
+    1. Upload a **chest X-ray image** 📤  
+    2. Wait for the **AI model to analyze** 🧠  
+    3. Get a **prediction along with confidence score** 🏥  
+    """)
 
-# ---- MAIN UI ----
-st.title("🩺 Medi Assist - Pneumonia Detection")
+def main_ui():
 
-# File uploader
-uploaded_file = st.file_uploader("Upload a Chest X-ray Image", type=["jpg", "png", "jpeg"])
-
-if uploaded_file:
-    # Create a directory if not exists
-    os.makedirs("sample_images", exist_ok=True)
+    # ---- MAIN UI ----
+    st.title("🩺 Medi Assist")
     
-    # Save uploaded file
-    file_path = os.path.join("sample_images", uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+        # Dropdown for disease selection
+    option = st.selectbox("Select Disease to Detect:", ["Detect Pneumonia", "Detect Fracture"])
 
-    # Display the uploaded image with proper alignment
-    col1, col2 = st.columns([1, 1])
-    with col1:
-        st.image(file_path, caption="🖼 Uploaded Image", use_container_width=True)
+    # File uploader
+    uploaded_file = st.file_uploader("Upload a Chest X-ray Image", type=["jpg", "png", "jpeg"])
 
-    with col2:
-        # Predict the result
-        label, confidence = predict_image(file_path)
+    if uploaded_file:
+        # Create a directory if not exists
+        os.makedirs("sample_images", exist_ok=True)
+        
+        # Save uploaded file
+        file_path = os.path.join("sample_images", uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
 
-        # Show the result
-        st.markdown("#### 🔍 Prediction Result")
-        st.success(f"**{label}**")
-        st.write(f"**Confidence Score:** {confidence:.2f}")
+        # Display the uploaded image with proper alignment
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.image(file_path, caption="🖼 Uploaded Image", use_container_width=True)
 
-# ---- FOOTER ----
-st.markdown("---")
-st.markdown(
-    """
-    <p style="text-align:center;">
-        Developed by  <a href="https://www.linkedin.com/in/mohit-dwivedi13" target="_blank">Mohit Dwivedi</a>
-        ,<a href="https://www.linkedin.com/in/charchilraj" target="_blank">Charchil Raj</a> and 
-          <a href="https://www.linkedin.com/in/suryanshrai011" target="_blank">Suryansh Rai</a> | 
-        © 2025 Medi Assist
-    </p>
-    """,
-    unsafe_allow_html=True
-)
+        with col2:
+            # Perform prediction based on selection
+            st.markdown("#### 🔍 Prediction Result")
+            if option == "Detect Pneumonia":
+                label, confidence = predict_pneumonia_image(file_path)
+                st.success(f"**{label}**")
+                st.write(f"**Confidence Score:** {confidence:.2f}")
+            # elif option == "Detect Skin Cancer":
+            #     benign_prob, malignant_prob = predict_skin_image(file_path)
+            #     st.write(f"Probablity of cancer being Benign : {benign_prob:.2f}")
+            #     st.write(f"Probability of cancer being Malignant : **{malignant_prob:.2f}**")
+            elif option == "Detect Fracture":
+                fracture_prediction = classify_fracture(image_path=file_path)
+                st.write(f"**{fracture_prediction}**")
+
+
+    # ---- FOOTER ----
+    st.markdown("---")
+    st.markdown(
+        """
+        <p style="text-align:center;">
+            Developed by  <a href="https://www.linkedin.com/in/mohit-dwivedi13" target="_blank">Mohit Dwivedi</a>
+            ,<a href="https://www.linkedin.com/in/charchilraj" target="_blank">Charchil Raj</a> and 
+            <a href="https://www.linkedin.com/in/suryanshrai011" target="_blank">Suryansh Rai</a> | 
+            © 2025 Medi Assist
+        </p>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    
+    
+    
+def main():
+    configure_page()
+    top_bar()
+    main_ui()
+    
+    
+    
+if __name__ == "__main__":
+    main()
--- module.py
+++ module.py
@@ -1,8 +1,16 @@
 import os
 import cv2
 import numpy as np
+import zipfile
+import io
+import tempfile
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+import torch
+from PIL import Image
+
 
 # Get absolute path to the model
 model_path = os.path.join("Model", "Medical_Assistant.h5")
@@ -18,7 +26,7 @@ if not os.path.exists(model_path):
 model = load_model(model_path)
 
 # Function to preprocess the image
-def preprocess_image(image_path):
+def preprocess_pneumonia_image(image_path):
     img_size = 150  # Model input size
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read as grayscale
     img = cv2.resize(img, (img_size, img_size))  # Resize to 150x150
@@ -28,8 +36,84 @@ def preprocess_image(image_path):
     return img
 
 # Function to make prediction
-def predict_image(image_path):
-    processed_img = preprocess_image(image_path)
+def predict_pneumonia_image(image_path):
+    processed_img = preprocess_pneumonia_image(image_path)
     prediction = model.predict(processed_img)[0][0]  # Get prediction score
     label = "PNEUMONIA" if prediction > 0.5 else "NORMAL"
     return label, prediction
+
+# Function for which will unzip the skin cancer detector model and process the image for the 
+
+skin_cancer_detector_path = os.path.join("Model","model.zip")
+
+# def load_skin_model_from_zip(zip_path):
+#     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+#         with tempfile.TemporaryDirectory() as temp_dir:
+#             zip_ref.extractall(temp_dir)  # Extract to a temporary directory
+
+#             # Find the model file inside extracted files
+#             model_path = os.path.join(temp_dir, "model.h5")  # Change this if needed
+
+#             # Load the model
+#             model = tf.keras.models.load_model(model_path)
+    
+#     return model
+
+# def process_skin_image(image_path, target_size=(224, 224)):
+#     """
+#     Processes an image for model prediction.
+
+#     Parameters:
+#         image_path (str): Path to the image file.
+#         target_size (tuple): Desired image size (default: (224, 224)).
+
+#     Returns:
+#         numpy.ndarray: Preprocessed image array.
+#     """
+#     img = load_img(image_path, target_size=target_size)
+#     img_array = img_to_array(img) / 255.0
+#     img_array = np.expand_dims(img_array, axis=0)
+#     return img_array
+
+# def predict_skin_image(image_path):
+#     """
+#     Loads the model from zip, processes the image, and returns predictions.
+
+#     Parameters:
+#         model_zip_path (str): Path to the zipped model file.
+#         image_path (str): Path to the image file for prediction.
+
+#     Returns:
+#         numpy.ndarray: Prediction output from the model.
+#     """
+#     model = load_skin_model_from_zip(skin_cancer_detector_path)
+#     img_array = process_skin_image(image_path)
+#     benign_prob, malignant_prob = model.predict(img_array)[0]
+#     return benign_prob,malignant_prob
+
+
+
+# this is for the Bone fracture classification
+
+fracture_processor = AutoImageProcessor.from_pretrained("Heem2/bone-fracture-detection-using-xray")
+fracture_model = AutoModelForImageClassification.from_pretrained("Heem2/bone-fracture-detection-using-xray")
+
+def classify_fracture(image_path):
+    # Load image
+    image = Image.open(image_path).convert("RGB")  # Ensure RGB mode
+
+    # Preprocess the image
+    inputs = fracture_processor(images=image, return_tensors="pt")  # Convert to PyTorch tensor
+
+    # Perform inference
+    with torch.no_grad():
+        outputs = fracture_model(**inputs)
+
+    # Get predicted class
+    logits = outputs.logits
+    predicted_class = torch.argmax(logits, dim=-1).item()
+
+    # Mapping the class index to a label
+    class_labels = fracture_model.config.id2label  # Hugging Face model label mapping
+
+    return class_labels[predicted_class]  # Return the predicted label
